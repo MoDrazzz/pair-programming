49b1f0878d77df1e08a5b369138ee4d9363bd3a9
Grzegorz Pawlik <grzegorz8.p@gmail.com>
2024-04-25 16:33:23 +0200

    chore: initialize project

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <main className="bg-zinc-800 text-zinc-50 grid place-items-center h-screen">
+      <h1 className='text-red-500'>Hello</h1>
+    </main>
+  )
+}
+
+export default App
