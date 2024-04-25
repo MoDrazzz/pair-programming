9221efec866d26cf117a14317b58790a57b854a6
Grzegorz Pawlik <grzegorz8.p@gmail.com>
2024-04-25 17:00:48 +0200

    feat: display content on EpisodePage

--- src/components/EpisodePage.tsx
+++ src/components/EpisodePage.tsx
@@ -12,9 +12,22 @@ const EpisodePage = () => {
     .then(data => setEpisodeData(data))
   }, [episodeId])
 
+  if(!episodeData) {
+    return <h1>Loading...</h1>
+  }
+
+  const episodeDate = new Date(episodeData.created).toLocaleString('en-us');
+
   return (
     <main className="grid place-items-center h-screen">
+      <div>
       <h1>Episode {episodeData?.name}</h1>
+      <ul>
+        <li><strong>Air Date:</strong> {episodeData.air_date}</li>
+        <li><strong>Episode:</strong> {episodeData.episode}</li>
+        <li><strong>Created:</strong> {episodeDate}</li>
+      </ul>
+      </div>
     </main>
   )
 };
