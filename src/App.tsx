07f76e430ca1b09398bc8eddc072b4c2fcb62861
Grzegorz Pawlik <grzegorz8.p@gmail.com>
2024-04-25 16:40:53 +0200

    feat: fetch episodes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
+import { useEffect, useState } from "react"
+import { Episode } from "./types";
+
 function App() {
+  const [, setEpisodes] = useState<Episode[]>([])
+
+  useEffect(() => {
+    fetch("https://rickandmortyapi.com/api/episode")
+      .then(res => res.json())
+      .then(data => {
+        setEpisodes(data.results);
+      })
+  }, [])
+
   return (
     <main className="bg-zinc-800 text-zinc-50 grid place-items-center h-screen">
-      <h1 className='text-red-500'>Hello</h1>
+      Hello
     </main>
   )
 }
